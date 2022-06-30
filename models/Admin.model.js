d9ae467902ec0100201ba0b431d60ec1b39f9d4b
Mapy Villalobos <mapyvillalobos@Mapys-MacBook-Pro.local>
2022-06-29 22:33:46 -0500

    change on user model

--- models/Admin.model.js
+++ models/Admin.model.js
@@ -3,38 +3,11 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const AdminSchema = new Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-    surname: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    profile_pic: {
-      type: String,
-      default:
-        "https://res.cloudinary.com/dhgfid3ej/image/upload/v1558806705/asdsadsa_iysw1l.jpg",
-    },
-    password: String,
-    role: {
-      type: String,
-      enum: ["ADMIN", "COACH", "PLAYER"],
-      default: "PLAYER",
-    },
-
-    //// datos de la liga, no del adim
     league_name: {
       type: String,
-      required: true,
       unique: true,
     },
-    league_pic: {
+    league_logo: {
       type: String,
       default:
         "https://res.cloudinary.com/dhgfid3ej/image/upload/v1558806705/asdsadsa_iysw1l.jpg",
--- models/Coach.model.js
+++ models/Coach.model.js
@@ -3,44 +3,14 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const CoachSchema = new Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-    surname: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    profile_pic: {
-      type: String,
-      default:
-        "https://res.cloudinary.com/dhgfid3ej/image/upload/v1558806705/asdsadsa_iysw1l.jpg",
-    },
-    password: String,
-    role: {
-      type: String,
-      enum: ["ADMIN", "COACH", "PLAYER"],
-      default: "PLAYER",
-    },
-
-    //// datos del equipo, no del coach
     team_name: {
       type: String,
-      required: true,
       unique: true,
     },
-    team_pic: {
+    team_logo: {
       type: String,
       default:
         "https://res.cloudinary.com/dhgfid3ej/image/upload/v1558806705/asdsadsa_iysw1l.jpg",
-    },
-    country: {
-      type: String, // lo hacemo enum? Ponemos países o estados?
     },
   },
   {
