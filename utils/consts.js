eb1397eedd6e694da968034a6873700bad28c00b
Carlos A Perez <carlosa.ps@outlook.com>
2023-10-31 22:04:52 -0600

    connection to Atlas

--- utils/consts.js
+++ utils/consts.js
@@ -1,3 +1,3 @@
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/tochido";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://carlosaps:wimw3A37JxzVcYNT@tochido.zfqwczr.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = MONGO_URI;
